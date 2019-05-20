5f91beac280addee3e54745a3e5ef388cedc62f8
runapps <apps@acornsoft.io>
2019-05-20 18:39:34 +0900

    echo mongo-url

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+set -e
 current_time=`date '+%Y%m%d_%H%M%S'`
 
-echo "$current_time"
+echo "${MONGO_HOST}:${MONGO_PORT} - ${current_time}"
 mongodump --host ${MONGO_HOST} --port ${MONGO_PORT} -u ${MONGO_USER} -p ${MONGO_PASS} --authenticationDatabase ${MONGO_AUTH_DB} --db ${MONGO_DB} --gzip --archive > /root/dumptest.gz.archive.${current_time}
