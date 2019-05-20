edb33633ebbfc9329225cd4c01a34f801ef0adf5
runapps <apps@acornsoft.io>
2019-05-20 18:20:42 +0900

    add a mongodump script

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 current_time=`date '+%Y%m%d_%H%M%S'`
 
 echo "$current_time"
-mongodump --host ${MONGO_HOST} --port ${MONGO_PORT} -u ${MONGO_USER} -p ${MONGO_PASS} --authenticationDatabase ${MONGO_AUTH_DB}
+mongodump --host ${MONGO_HOST} --port ${MONGO_PORT} -u ${MONGO_USER} -p ${MONGO_PASS} --authenticationDatabase ${MONGO_AUTH_DB} --db ${MONGO_DB} --gzip --archive > /root/dumptest.gz.archive.${current_time}
