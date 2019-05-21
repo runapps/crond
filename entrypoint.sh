558f66d1e072b07de3e75a6de95af4667a4e93b5
runapps <apps@acornsoft.io>
2019-05-21 15:45:25 +0900

    스케쥴 환경변수 처리

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ sed -i 's/${MONGO_DB}/'${MONGO_DB}'/g' script.sh
 sed -i 's/${FILE_PREFIX}/'${FILE_PREFIX}'/g' script.sh
 sed -i 's/${FILE_EXT}/'${FILE_EXT}'/g' script.sh
 sed -i 's/${FILE_RETENTION}/'${FILE_RETENTION}'/g' script.sh
+sed -i 's/\* \* \* \* \*/'${CRON_SCHEDULE}'/g' /etc/cron.d/cronjob
 
 /usr/bin/crontab /etc/cron.d/cronjob
 cron -f
