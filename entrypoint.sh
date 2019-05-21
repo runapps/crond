29b5282ce38a0430f13b929a84ad00f64de568c6
runapps <apps@acornsoft.io>
2019-05-21 17:39:21 +0900

    FILE_PATH 치환 오류 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+# sed는 특수문자 앞에 \를 붙여야한다
+# bash 변수문자열치환 방법 ${variable//search/replace}
+# replace / -> \/ for sed
+file_path=${FILE_PATH//\//\\/}
+cron_schedule=${CRON_SCHEDULE//\//\\/}
+
 mkdir -p ${FILE_PATH}
 sed -i 's/${MONGO_HOST}/'${MONGO_HOST}'/g' script.sh
 sed -i 's/${MONGO_PORT}/'${MONGO_PORT}'/g' script.sh
@@ -6,11 +13,11 @@ sed -i 's/${MONGO_USER}/'${MONGO_USER}'/g' script.sh
 sed -i 's/${MONGO_PASS}/'${MONGO_PASS}'/g' script.sh
 sed -i 's/${MONGO_AUTH_DB}/'${MONGO_AUTH_DB}'/g' script.sh
 sed -i 's/${MONGO_DB}/'${MONGO_DB}'/g' script.sh
-sed -i 's/${FILE_PATH}/'${FILE_PATH}'/g' script.sh
+sed -i 's/${FILE_PATH}/'${file_path}'/g' script.sh
 sed -i 's/${FILE_PREFIX}/'${FILE_PREFIX}'/g' script.sh
 sed -i 's/${FILE_EXT}/'${FILE_EXT}'/g' script.sh
 sed -i 's/${FILE_RETENTION}/'${FILE_RETENTION}'/g' script.sh
-sed -i 's/\* \* \* \* \*/'"${CRON_SCHEDULE}"'/g' /etc/cron.d/cronjob
+sed -i 's/\* \* \* \* \*/'"${cron_schedule}"'/g' /etc/cron.d/cronjob
 
 /usr/bin/crontab /etc/cron.d/cronjob
 cron -f
