ca9d65b0b71d6e872f54d95ea6f8254b7dd1f6cf
runapps <apps@acornsoft.io>
2019-05-21 16:50:56 +0900

    FILE_PATH 변수 추가

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+mkdir -p ${FILE_PATH}
 sed -i 's/${MONGO_HOST}/'${MONGO_HOST}'/g' script.sh
 sed -i 's/${MONGO_PORT}/'${MONGO_PORT}'/g' script.sh
 sed -i 's/${MONGO_USER}/'${MONGO_USER}'/g' script.sh
 sed -i 's/${MONGO_PASS}/'${MONGO_PASS}'/g' script.sh
 sed -i 's/${MONGO_AUTH_DB}/'${MONGO_AUTH_DB}'/g' script.sh
 sed -i 's/${MONGO_DB}/'${MONGO_DB}'/g' script.sh
+sed -i 's/${FILE_PATH}/'${FILE_PATH}'/g' script.sh
 sed -i 's/${FILE_PREFIX}/'${FILE_PREFIX}'/g' script.sh
 sed -i 's/${FILE_EXT}/'${FILE_EXT}'/g' script.sh
 sed -i 's/${FILE_RETENTION}/'${FILE_RETENTION}'/g' script.sh
--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ retention=${FILE_RETENTION}
 tailnum=$((retention + 1))
 
 echo "${MONGO_HOST}:${MONGO_PORT} - ${current_time}"
-mongodump --host ${MONGO_HOST} --port ${MONGO_PORT} -u ${MONGO_USER} -p ${MONGO_PASS} --authenticationDatabase ${MONGO_AUTH_DB} --db ${MONGO_DB} --gzip --archive > /root/${FILE_PREFIX}.${FILE_EXT}.${current_time}
+mongodump --host ${MONGO_HOST} --port ${MONGO_PORT} -u ${MONGO_USER} -p ${MONGO_PASS} --authenticationDatabase ${MONGO_AUTH_DB} --db ${MONGO_DB} --gzip --archive > ${FILE_PATH}/${FILE_PREFIX}.${FILE_EXT}.${current_time}
 
-find /root -name '${FILE_PREFIX}.${FILE_EXT}.*' -type f | sort -r | tail -n +${tailnum} | xargs -i echo "rm -rf {}"
-find /root -name '${FILE_PREFIX}.${FILE_EXT}.*' -type f | sort -r | tail -n +${tailnum} | xargs rm -rf
+find ${FILE_PATH} -name '${FILE_PREFIX}.${FILE_EXT}.*' -type f | sort -r | tail -n +${tailnum} | xargs -i echo "rm -rf {}"
+find ${FILE_PATH} -name '${FILE_PREFIX}.${FILE_EXT}.*' -type f | sort -r | tail -n +${tailnum} | xargs rm -rf
