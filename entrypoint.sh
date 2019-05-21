7195ded8502f3e9aacd7f355ad82fc455f5ccd40
runapps <apps@acornsoft.io>
2019-05-21 14:26:28 +0900

    edit script.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+sed -i 's/{{MONGO_HOST}}/${MONGO_HOST}/g' script.sh
+sed -i 's/{{MONGO_PORT}}/${MONGO_PORT}/g' script.sh
+sed -i 's/{{MONGO_USER}}/${MONGO_USER}/g' script.sh
+sed -i 's/{{MONGO_PASS}}/${MONGO_PASS}/g' script.sh
+sed -i 's/{{MONGO_AUTH_DB}}/${MONGO_AUTH_DB}/g' script.sh
+sed -i 's/{{MONGO_DB}}/${MONGO_DB}/g' script.sh
+sed -i 's/{{FILE_PREFIX}}/${FILE_PREFIX}/g' script.sh
+sed -i 's/{{FILE_EXT}}/${FILE_EXT}/g' script.sh
+
 /usr/bin/crontab /etc/cron.d/cronjob
 cron -f
